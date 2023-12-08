ae4662f00c294986e0ef7687e51562aaa598d240
Niklas Wagner <Skaro@Skaronator.com>
2023-12-08 18:59:38 +0100

    Refactor release and build process

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,4 @@
-FROM python:3.12.0-alpine3.18 as BUILDER
-
-WORKDIR /workdir
-COPY ./builder .
-COPY tools.yaml .
-
-RUN pip install -r requirements.txt
-RUN python main.py --output-dir=/output --tools-yaml=./tools.yaml
-
 FROM scratch as BASE
 
 WORKDIR /app
-COPY --from=BUILDER /output /app
+COPY ./dist /app
--- builder/main.py
+++ builder/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 import yaml
 import argparse
 import platform
@@ -31,12 +32,20 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Process tools from a YAML file.')
     parser.add_argument('--tools-yaml', help='Path to the tools YAML file', dest='tools', default='../tools.yaml')
-    parser.add_argument('--output-dir', help='Path where the tools are installed to', dest='output', default='/tmp/output')
+    parser.add_argument('--output-dir', help='Path where the tools are installed to', dest='output', default='../dist')
 
     args = parser.parse_args()
 
     with open(args.tools, 'r') as file:
-        tools = yaml.safe_load(file)
+        tools: ToolList = yaml.safe_load(file)
 
     for tool in tools['tools']:
         process_tool(tool, args.output)
+
+    output_file = os.path.join(args.output, 'versions.txt')
+    with open(output_file, 'w') as output_file:
+        sorted_tools = sorted(tools['tools'], key=lambda x: x['name'])
+        for tool in sorted_tools:
+            name = tool['name']
+            version = tool['version']
+            output_file.write(f"{name}: {version}\n")
