cb0382f7c83f9d02f78fd5bffd3adfda7dfcb122
Niklas Wagner <Skaro@Skaronator.com>
2023-12-29 13:10:10 +0100

    Refactor toolbuilder

---
+++ builder/dockerfile/__init__.py
@@ -0,0 +1,23 @@
+import os
+
+
+def generate_dockerfile(tools_dir, dockerfile_path):
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    dockerfile = os.path.join(script_directory, "Dockerfile")
+    with open(dockerfile, 'r') as existing_file:
+        dockerfile_content = existing_file.read()
+
+    files = [f for f in os.listdir(tools_dir) if os.path.isfile(os.path.join(tools_dir, f))]
+    files.sort(key=lambda f: os.path.getsize(os.path.join(tools_dir, f)), reverse=True)
+    top_files = files[:25]
+
+    # Generate COPY commands for the top 10 files
+    dockerfile_content += "\n" + "\n".join([f"COPY {f} /app/{f}" for f in top_files])
+
+    # Generate a single COPY command for all other files
+    other_files = set(files) - set(top_files)
+    if other_files:
+        dockerfile_content += "\n" + f"COPY {' '.join(other_files)} /app/"
+
+    with open(dockerfile_path, 'w') as output_file:
+        output_file.write(dockerfile_content)
--- builder/main.py
+++ builder/main.py
@@ -2,59 +2,25 @@
 import os
 import yaml
 import argparse
-from builder.types import Tool, ToolList
-from builder.fetch import fetch_tool
-from builder.verify import verify_tool
-
-DOCKER_IMAGE = "ghcr.io/skaronator/devops-toolbox:latest"
-
-
-def process_tool(tool: Tool, output_dir) -> str | None:
-    name = tool.name
-    download_url = tool.get_download_url()
-
-    if download_url is None:
-        print(f"{'=' * 25} Skipping {name} - Download not available for this plattform {'=' * 25}")
-        return
-
-    print(f"{'=' * 25} Installing {name} @ {tool.version} {'=' * 25}")
-
-    fetch_tool(output_dir, download_url, name)
-    verify_tool(output_dir, tool.verify_command)
-
-    print(f"{'=' * 25} Successfully installed {name}! {'=' * 25}")
-
-    cmd = [
-        "docker",
-        "run",
-        "--tty" if tool.tty else "",
-        "--interactive" if tool.interactive else "",
-        "--rm",
-        "-e", "HOME",
-        "-e", "USER",
-        "-v", "$PWD:/workdir",
-        tool.docker_command,
-        DOCKER_IMAGE,
-        name,
-    ]
-    # remove empty strings from cmd
-    cmd = [x for x in cmd if x.strip()]
-    alias_command = f"alias '{name}'='{' '.join(cmd)}'"
-    return alias_command
-
+from tools import Tool, DOCKER_IMAGE
+from dockerfile import generate_dockerfile
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Process tools from a YAML file.')
-    parser.add_argument('--tools-yaml', help='Path to the tools YAML file', dest='tools', default='../tools.yaml')
-    parser.add_argument('--output-dir', help='Path where the tools are installed to', dest='output', default='../dist')
+    parser.add_argument('--tools-yaml', help='Path to the tools YAML file', dest='tools', default='./tools.yaml')
+    parser.add_argument('--output-dir', help='Path where the tools are installed to', dest='output', default='./dist')
+    parser.add_argument('--dockerfile', help='Path where the dockerfile will be generated to', dest='dockerfile', default='./Dockerfile')
 
     args = parser.parse_args()
+
+    output_dir = os.path.abspath(args.output)
+    dockerfile_path = os.path.abspath(args.dockerfile)
 
     with open(args.tools, 'r') as file:
         tools_data = yaml.safe_load(file)
 
-    tools: ToolList = [Tool(**tool_data) for tool_data in tools_data['tools']]
+    tools = [Tool(output_dir=output_dir, **tool_data) for tool_data in tools_data['tools']]
 
     all_alias = f"""#!/bin/env sh
 alias toolbox-update='
@@ -70,7 +36,7 @@ alias toolbox-update='
 """
 
     for tool in tools:
-        alias = process_tool(tool, args.output)
+        alias = tool.process()
         if alias:
             all_alias += alias + "\n"
 
@@ -79,3 +45,5 @@ alias toolbox-update='
         file.write(all_alias)
 
     os.chmod(load_alias_file, 0o755)
+
+    generate_dockerfile(output_dir, dockerfile_path)
